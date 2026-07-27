c95b242158daf0ec13f1967574a548c2158421a6
Benyahia ibrahim elkhalil <khalilbenyahia2@gmail.com>
2026-07-27 23:30:35 +0100

    migrate to prisma 6

--- src/config/DB.ts
+++ src/config/DB.ts
@@ -1,20 +1,10 @@
 import { PrismaClient } from "../generated/prisma/client";
 
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import ENV from "./ENV";
 
 
-const adapter = new PrismaMariaDb({
-    host:ENV.DB_HOST,
-    port: Number(ENV.DB_PORT),
-    user: ENV.DB_USER,
-    password: ENV.DB_PASSWORD,
-    database: ENV.DB_NAME,
-});
 
 
 const prisma = new PrismaClient({
-    adapter,
     omit:{
         
         user:{
